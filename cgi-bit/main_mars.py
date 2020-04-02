14c91dbd8aea0c6796ca365aee64079802c96e36
kseniya <devksuksu@mail.ru>
2020-04-02 17:14:56 +0300

    autorization

---
+++ cgi-bit/main_mars.py
@@ -0,0 +1,111 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, BooleanField
+from wtforms.validators import DataRequired
+from wtforms.fields.html5 import EmailField
+from flask import Flask, redirect, render_template, session
+from data import db_session
+from data.jobs import Jobs
+from data.users import User
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class RegisterForm(FlaskForm):
+    email = EmailField('Login / email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    password_again = PasswordField('Repeat password', validators=[DataRequired()])
+    name = StringField('Name', validators=[DataRequired()])
+    surname = StringField('Surname', validators=[DataRequired()])
+    age = IntegerField('Age', validators=[DataRequired()])
+    position = StringField('Position', validators=[DataRequired()])
+    speciality = StringField('Speciality', validators=[DataRequired()])
+    address = StringField('Address', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    db_session.global_init('db/mars_explorer.sqlite')
+    session = db_session.create_session()
+    return session.query(User).get(user_id)
+
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect("/")
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    global session
+    form = LoginForm()
+    if form.validate_on_submit():
+        db_session.global_init('db/mars_explorer.sqlite')
+        session = db_session.create_session()
+        user = session.query(User).filter(User.email == form.email.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user, remember=form.remember_me.data)
+            return redirect("/")
+        return render_template('login1.html',
+                               message="Неправильный логин или пароль",
+                               form=form)
+    return render_template('login1.html', title='Авторизация', form=form)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        if form.password.data != form.password_again.data:
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Пароли не совпадают")
+        db_session.global_init('db/mars_explorer.sqlite')
+        session = db_session.create_session()
+        if session.query(User).filter(User.email == form.email.data).first():
+            return render_template('register.html', title='Регистрация',
+                                   form=form,
+                                   message="Такой пользователь уже есть")
+        user = User(
+            name=form.name.data,
+            email=form.email.data,
+            surname=form.surname.data,
+            age=form.age.data,
+            position=form.position.data,
+            speciality=form.speciality.data,
+            address=form.address.data
+        )
+        user.set_password(form.password.data)
+        session.add(user)
+        session.commit()
+    return render_template('register.html', title='Registration', form=form)
+
+
+@app.route('/')
+def ind():
+    if current_user.is_authenticated:
+        jobs = session.query(Jobs).filter(
+            (Jobs.user == current_user) | (not Jobs.is_finished))
+    else:
+        jobs = session.query(Jobs).filter(not Jobs.is_finished)
+    return render_template("index.html", news=jobs)
+
+
+if __name__ == '__main__':
+    app.run(port=80, host='127.0.0.1')
